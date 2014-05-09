ced09a9b6fec3f98c0673e036fc02b3e69843f88
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-09 15:32:54 -0400

    area of triangle
    found area of a triangle

--- day 3 files/Examples/js/script.js
+++ day 3 files/Examples/js/script.js
@@ -10,3 +10,11 @@ var birthYear = 1985;
 //create a variable to hold the age
 var age = 2014 - birthYear;
 console.log(age);
+//math
+//+ - x /
+//find the are of a triangle
+// base * hight *1/2
+var base = 4;
+var height = 5;
+var areaTriangle = base*height*.5;
+console.log(areaTriangle);
