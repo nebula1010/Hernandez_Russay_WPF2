cfdd133b38d69cf58f911cafa7322dc352897689
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-09 15:24:17 -0400

    add birth year
    figured out birth year

--- day 3 files/Examples/js/script.js
+++ day 3 files/Examples/js/script.js
@@ -3,4 +3,10 @@ Russay Hernandez
 WPF 1405
 Day 3 class examples
  */
-alert("test")
+//alert("test")
+//find out how old we are
+//create a variable for birth year
+var birthYear = 1985;
+//create a variable to hold the age
+var age = 2014 - birthYear;
+console.log(age);
