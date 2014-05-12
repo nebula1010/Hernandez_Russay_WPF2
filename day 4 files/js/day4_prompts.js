8d7f493860ae8ce6fb35946dbb86e293e9d5eac3
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-12 16:15:19 -0400

    prompt lesson
    learned how to use prompts

---
+++ day 4 files/js/day4_prompts.js
@@ -0,0 +1,30 @@
+/**
+ * Created by russayhernandez on 5/12/14.
+ */
+/*
+russay hernandez
+WPF section 1
+day 4 prompts
+ */
+//alert("hello")
+// prompting use for information
+// prompt("enter your year of birth");
+// capture the information the use provide
+//can you say...variable
+//var userInput = prompt("enter your birth year:");
+//console.log(userInput);
+
+// prompt and explain to the user what we are actually doing.
+// prompt the use for a width and calculate the area of a triangle
+var width = prompt ("lets calculate the area of a triangle. \nplease enter a width");
+var height = prompt ("please enter a height");
+
+//find the area
+var area = width*height;
+console.log ("the area of your rectangle is "+area+"");
+alert("the area of your rectangle is "+area+"");
+
+// create a variable to hold the whole strings of an answer
+var result = "the area of a rectangle with a width of a "+width+" and a height of a "+height+" is "+area+".";
+console.log(result);
+alert(result);
