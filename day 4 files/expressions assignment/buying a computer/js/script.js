e4d5b3778bb9f79c8a606580013a9a72d3df55f2
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-14 13:58:42 -0400

    basic if statement
    learned the if statement in conjunction with booleans

---
+++ day 4 files/expressions assignment/buying a computer/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created by russayhernandez on 5/12/14.
+ */
+//Russay Hernandez
+//WPF section 1
+//expressions homework day 4
+
---
+++ day 5 files/js/day5_conditionals.js
@@ -0,0 +1,21 @@
+/**
+ * Created by russayhernandez on 5/14/14.
+ */
+//Russay Hernandez
+//WPF Section 1
+//basic conditionals
+// if statement ---
+//if a child is tall enough, print to the console that he can ride a roller coster
+var tallEnough = true;
+
+/*
+if(thing to evaluate){
+code to run if it is true!
+}
+ */
+if(tallEnough){
+    //this code will run if the kid is tall enough
+    console.log("you can ride the roller coster");
+
+}
+console.log("what comes after");
