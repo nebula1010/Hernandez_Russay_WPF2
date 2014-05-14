777ffedd56619a3a3460a4ea864a97a7369e15b3
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-14 14:29:22 -0400

    relational expressions
    asking if the kid is tall enough with numbers

--- day 5 files/js/day5_conditionals.js
+++ day 5 files/js/day5_conditionals.js
@@ -22,3 +22,19 @@ if(tallEnough){
     console.log("sorry kid isnt tall enoguh");
 }
 console.log("what comes after");
+
+
+//relational expression
+// if a child is not over 30 inches long then he cant ride
+
+
+var kidHeight = 30;
+
+var minHeight = 48;
+
+if(kidHeight>=minHeight){
+    console.log("you can ride!!")
+}else{
+    console.log("sorry, not tall enough")
+}
+console.log("what comes after")
