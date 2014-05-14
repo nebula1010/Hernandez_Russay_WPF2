a61c17c6b9eeeec254b8f57d1800bb3d85dc145d
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-14 16:47:28 -0400

    ternary conditions
    learned how to use it to set values to variables

--- day 5 files/js/day5_ternary.js
+++ day 5 files/js/day5_ternary.js
@@ -16,3 +16,28 @@ if(gpa>2.0){
     console.log("youre a failure")
 }
 
+//(condition)? true code : false code;:
+(gpa>2.0) ? console.log("congrats you can graduate"):console.log("sorry")
+
+//pick a book bases on a kids age
+// under 10 green eggs and ham- 10 and over gets time machine
+
+var age = 6;
+(age < 10) ? console.log("green eggs"):console.log("time machine");
+
+(age >= 10) ? console.log("time machine"):console.log("green eggs");
+
+// same the book game to a variable
+var book;
+
+book = (age < 10) ? "green egss and ham" : "time machine";
+console.log ("your kid gets the book" + book);
+
+if(age<10){
+    book = "green egss and ham";
+
+}else{
+    book= "time machine"
+}
+
+console.log("this is the if statement" + book);
