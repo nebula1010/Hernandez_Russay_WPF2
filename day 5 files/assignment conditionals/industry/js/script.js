a0bdc8e103569ecaecaf8ccee2acec5df8371749
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-19 19:30:51 -0400

    developed how to beat my opponent
    developed a conditional to find a way to beat my opponent

--- day 5 files/assignment conditionals/industry/js/script.js
+++ day 5 files/assignment conditionals/industry/js/script.js
@@ -8,7 +8,7 @@
 //im trying to find out if i am strong enough to beat the amount of weight that my friend can bench.
     // he currenctly benches 250 pounds, i need to find a formula to see if i can
 
-var amountICB = 246;
+var amountICB = 236;
 var goal = 250;
 var strength;
 
@@ -17,3 +17,18 @@ if(amountICB>goal){
 }else{
         strength = "not strong enough";
 }
+console.log("based on the results, i can determine that i am "+strength+" to beat my opponent" );
+
+//with the results in, i now have to determine if in a week i can beat his maxed out weight.
+
+oneWeek = 15;
+
+if (strength>goal || oneWeek){
+    var metGoal = "i can beat him"
+}else{
+    var metGoal = "i couldnt beat him"
+}
+console.log("after one week of working out, i have determined that "+metGoal+"");
+
+
+
