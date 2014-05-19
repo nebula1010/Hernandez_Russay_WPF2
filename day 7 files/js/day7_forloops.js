11b8a226372f4ebe7c4634a4481a9e586a73be05
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-19 15:05:31 -0400

    breaks
    stopping loops. even if its a

--- day 7 files/js/day7_forloops.js
+++ day 7 files/js/day7_forloops.js
@@ -13,3 +13,17 @@ for(var i = 1; i <= 20; i+=4){
 
     console.log("i= "+i);
 }
+//breaks
+//breaks- it stops the loop anywhere it is
+
+
+for(var k = 0; k < 5; k++){
+
+    console.log("k = "+k);
+
+
+    if(k===3){
+        console.log("your number is 3")
+        break;
+    }
+}
