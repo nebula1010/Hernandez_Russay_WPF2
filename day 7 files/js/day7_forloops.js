17106f23f29a236cde7c92d9dccea4c8fd3d45e6
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-19 14:49:50 -0400

    for loop
    easier than wild loops.

---
+++ day 7 files/js/day7_forloops.js
@@ -0,0 +1,15 @@
+/**
+ * Created by russayhernandez on 5/19/14.
+ */
+//Russay Hernandez
+//WPF section 1
+//for loops
+
+
+//basic for loop
+//for(initialization; conditions to test; change variable)
+
+for(var i = 1; i <= 20; i+=4){
+
+    console.log("i= "+i);
+}
