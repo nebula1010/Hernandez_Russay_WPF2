1116e7166f9841e89889dbbde64a35b88b1b692e
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-19 14:05:10 -0400

    basic wild loop
    created a loop

--- day 4 files/expressions assignment/assignment 3/js/script.js
+++ day 4 files/expressions assignment/assignment 3/js/script.js
@@ -3,7 +3,7 @@
  */
 //Russay Hernandez
 //WPF section 1
-    // create an array descriving how many times you go to the bathroom every week. given that you start the array at monday and end in sunday
+    // create an array describing how many times you go to the bathroom every week. given that you start the array at monday and end in sunday
 
 var timesYG=[7,8,11,11,14,8,9];
 // now calculate the total amount of times you went to the bathroom
@@ -11,5 +11,3 @@ var total = timesYG[0] + timesYG[1] + timesYG[2] + timesYG[3] + timesYG[4] + tim
 //what was the average times you went to the bathroom per day
 var average = total/timesYG.length;
 console.log("last week i went to the bathroom "+total+" times. that was about "+average+" times a day");
-
-console.log(timesYG);
---
+++ day 7 files/js/day7_wildLoops.js
@@ -0,0 +1,23 @@
+/**
+ * Created by russayhernandez on 5/19/14.
+ */
+//alert("hello");
+
+//basic wild loop
+//wild loops loops through a block of code as long as the condition is true
+
+//initialize the counter variable
+var counter = 0;
+
+//now we set up wild loop
+
+while(counter<200){//condition- loops unitl false
+    console.log("something just never end");
+    //change the value of the counter variable
+    //or it will run forever;;not good
+    console.log("the counter is "+counter+"")
+    counter++;
+
+}
+console.log("ending counter "+counter);
+
