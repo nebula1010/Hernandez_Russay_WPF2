cc4e33fee2e2bed851cd2ab3ba9590d2905077e8
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-19 20:30:05 -0400

    industry assignment
    i created a questioner to determine how many people in my field for for
    clients or a company

---
+++ day 5 files/assignment conditionals/industry/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Created by russayhernandez on 5/19/14.
+ */
+//Russay Hernandez
+//WPF section 1
+//Assignment conditionals - industry
+
+//im creating a questioner online to ask some of the other people in my field if they work for clients or a company
+    //the real goal of these questions is to determine how many clients do web developers normally have in order to
+    // get a feel of what i should do when i graduate school 
+
+var workOption = prompt("clients or company");
+
+if(workOption == "clients"){
+totalClients = prompt("how many clients do you have")
+    console.log("After a questioner, i have determined that the ones that worked individually, had "+totalClients+" clients" +
+        ". ")
+}else if(workOption == "company"){
+    var company = "worked for a company"
+console.log("from the questioner i took, these individual stated they worked for a company")
+
+}
+
+
