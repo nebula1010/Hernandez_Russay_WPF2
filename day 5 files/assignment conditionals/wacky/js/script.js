db4054ad2cfd26567c5cd5e1fe00710cb1775042
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-19 21:57:45 -0400

    assignment conditionals
    created a conditional prompting people what their iq was. then i sent
    them an alert with what their IQ is on the national scale. then i
    determined if they should, must or shouldn’t be procreating

---
+++ day 5 files/assignment conditionals/wacky/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by russayhernandez on 5/19/14.
+ */
+//Russay Hernandez
+//WPF section 1
+//assignment conditionals- wacky
+IQ = prompt("what is your IQ")
+
+if(IQ>=100){
+ IQ = "you can procreate";
+    alert("Congratulations, you're not an idiot, but nor are you smart");
+console.log("based on the your IQ, i can determine that "+IQ);
+}else if(IQ>=130){
+    IQ = "procreation is mandatory"
+    alert("you're a freaking genius")
+
+console.log("based on the your IQ, i can determine that "+IQ+" for you")
+}else(IQ<70);{
+    IQ = "must be castrated"
+    alert("i would insult you but you probably can't read :)")
+    console.log("based on the your IQ, i can determine that you "+IQ)
+}
+
