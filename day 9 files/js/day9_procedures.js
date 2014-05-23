391de74e5e1666e505b3835d91ebc2a2c0e22044
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-05-23 14:40:26 -0400

    procedures vs function
    procedures do not return a value

--- day 9 files/js/day9_procedures.js
+++ day 9 files/js/day9_procedures.js
@@ -3,7 +3,27 @@
  */
 //Russay Hernandez
 //WPF section 1
-//random numbers
+//Procedures
 
 
-alert("hello")
+function calcArea(){
+    var width = 6;
+    var height = 7;
+    var area = width*height;
+
+    return area;
+}
+
+//basic procedures
+//list of steps to do
+//NEVER returns value
+
+function calcAreap(){
+    var width = 5;
+var height = 6;
+var area = width * height
+
+ console.log(area);
+
+
+    }
